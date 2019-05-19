895a8bb2f91edded13cdbec4b008d4f2c1760999
Fady Samir Sadek <fadysamirsadek@gmail.com>
2019-05-19 04:59:12 +0200

    Add deploy option

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Fady Sadek`,
+    author: `Fady Sadek`,
+    description: `Fady Sadek Blog/`,
+    siteUrl: `http://fadysadek.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `fadysamirsadek`,
     },
   },
   plugins: [
